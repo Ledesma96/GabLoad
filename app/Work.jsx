0eb15b784b6d7a29232ed7d58862999383ea3f29
gabriel <gabriel.m.ledesma96@gmail.com>
2023-07-24 01:44:03 -0300

    aSDAS

--- app/Work.jsx
+++ app/Work.jsx
@@ -1,8 +1,9 @@
 "use client"
 import Image from 'next/image'
 import React from 'react'
-const works = [{id:2, url:"https://www.cook4souls.com", image:"/c4sMock.jpg", fecha:"19 de julio del 2023"},
-                {id:1,url: "https://www.itanflor.com/", image:"/Reg Hindley.jpg", fecha:"19 de junio del 2023"}
+const works = [{id:3, url:"https://cantinero.netlify.app/",image:"/cantineromook.jpg",fecha:"22 de julio del 2023"},
+                {id:2, url:"https://www.cook4souls.com", image:"/c4sMock.jpg", fecha:"19 de julio del 2023"},
+                {id:1, url: "https://www.itanflor.com/", image:"/Reg Hindley.jpg", fecha:"19 de junio del 2023"}
                 ]
 
 const Work = () => {
