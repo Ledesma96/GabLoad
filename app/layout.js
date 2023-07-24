4ad85408e87ba8e4b9ab2f93a22db7f86e450f6c
gabriel <gabriel.m.ledesma96@gmail.com>
2023-07-24 10:12:45 -0300

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import EndsWorks from './EndsWorks'
 import NavBarMin from './NavBarMin'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         </header>
         <EndsWorks></EndsWorks>
         {children}
+        <Analytics />
         <footer className='footer'>
           <secion className="footer__section">
             <h3 className='footer__section__h3'>¡Dejanos tu email si querés saber las últimas novedades!</h3>
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import EndsWorks from './components/EndsWorks'
 import NavBarMin from './components/NavBarMin'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         </header>
         <EndsWorks></EndsWorks>
         {children}
+        <Analytics />
         <footer className='footer'>
           <secion className="footer__section">
             <h3 className='footer__section__h3'>¡Dejanos tu email si querés saber las últimas novedades!</h3>
