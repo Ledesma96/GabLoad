c275aec2fafa8b1e1b24dc5be66ae1fb8e6a269a
Gabriel <gabriel.m.ledesma96@gmail.com>
2024-04-04 10:05:47 -0300

    footer atras

--- app/Pricing.jsx
+++ app/Pricing.jsx
@@ -88,7 +88,7 @@ const Pricing = () => {
 
   return (
     <div className="container my-24 px-6 mx-auto " id="pricing">
-      <section className="mb-32 text-gray-800" style={{marginLeft: "-100px"}}>
+      <section className="mb-32 text-gray-800 section-pricing" >
         <div
           id="pricing-block-5"
           className="background-radial-gradient text-center text-white"
